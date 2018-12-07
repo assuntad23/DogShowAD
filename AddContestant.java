ef17a20d219981560b2451b4d66c9002900b1766
Caroline Rublein <typhlluxe@gmail.com>
2018-12-07 12:43:46 -0500

    AddContestant Individual File Upload - CLR 12/7

---
+++ AddContestant.java
@@ -0,0 +1,13 @@
+package com.example.shaun.dogshow2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddContestant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contestant);
+    }
+}
